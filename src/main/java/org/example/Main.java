f8ab505f797a6124c97d08aaba35157f91b01439
simon8491 <chmel.simon@gmail.com>
2024-12-22 15:39:36 +0100

    System. added

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -11,8 +11,6 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
 
-import static java.lang.System.*;
-
 public class Main {
     static String[][] tasks;
 
@@ -20,15 +18,15 @@ public class Main {
 
         tasks = dataRetrieve("tasks.csv");
         options();
-        var input = new Scanner(in);
+        var input = new Scanner(System.in);
         while (input.hasNextLine()) {
             var option = input.nextLine();
             switch (option) {
                 case "exit":
                     storeData();
-                    out.println("Data were stored successfully");
-                    out.println(ConsoleColors.RED + "Sayonara!");
-                    exit(0);
+                    System.out.println("Data were stored successfully");
+                    System.out.println(ConsoleColors.RED + "Sayonara!");
+                    System.exit(0);
                     break;
                 case "add":
                     addTask();
@@ -40,21 +38,21 @@ public class Main {
                     taskList();
                     break;
                 default:
-                    out.println("Invalid option");
+                    System.out.println("Invalid option");
             }
             options();
         }
-        out.println(Arrays.deepToString(tasks));
+        System.out.println(Arrays.deepToString(tasks));
     }
 
     public static void options() {
         String[] options = {"add", "remove", "list", "exit"};
-        out.println(ConsoleColors.BLUE);
-        out.println("Please select the option for task manager:");
+        System.out.println(ConsoleColors.BLUE);
+        System.out.println("Please select the option for task manager:");
         for (var option : options) {
-            out.println(ConsoleColors.BLUE + option);
+            System.out.println(ConsoleColors.BLUE + option);
         }
-        out.println(ConsoleColors.RESET);
+        System.out.println(ConsoleColors.RESET);
     }
 
     public static String[][] dataRetrieve(String fileName) {
@@ -62,31 +60,31 @@ public class Main {
         String[][] dataTab = null;
 
         if (!Files.exists(file)) {
-            err.println("File does not exist.");
-            exit(0);
+            System.out.println("File does not exist.");
+            System.exit(0);
         }
         try {
             List<String> lines = Files.readAllLines(file);
             dataTab = new String[lines.size()][];
             for (int i = 0; i < lines.size(); i++) {
                 dataTab[i] = lines.get(i).split(",");
-                arraycopy(lines.get(i).split(","), 0, dataTab[i], 0, dataTab[i].length);
+                System.arraycopy(lines.get(i).split(","), 0, dataTab[i], 0, dataTab[i].length);
             }
         } catch (IOException e) {
-            err.println("Error reading file.");
-            e.printStackTrace(err);
+            System.err.println("Error reading file.");
+            e.printStackTrace(System.err);
         }
         return dataTab;
     }
 
     public static void addTask() {
 
-        var input = new Scanner(in);
-        out.println("Please enter the task description:");
+        var input = new Scanner(System.in);
+        System.out.println("Please enter the task description:");
         var taskDescription = input.nextLine();
-        out.println("Then, please enter the due date:");
+        System.out.println("Then, please enter the due date:");
         var dueDate = input.nextLine();
-        out.println("And lastly, please enter the importance of the task (true/false):");
+        System.out.println("And lastly, please enter the importance of the task (true/false):");
         var importance = input.nextLine();
 
 
@@ -99,23 +97,23 @@ public class Main {
 
     public static void taskList() {
         for (int i = 0; i < tasks.length; i++) {
-            out.print(i + 1 + ". ");
+            System.out.print(i + 1 + ". ");
             for (int j = 0; j < tasks[i].length; j++) {
-                out.print(tasks[i][j] + " ");
+                System.out.print(tasks[i][j] + " ");
             }
-            out.println();
+            System.out.println();
         }
     }
 
     public static void removeTask() {
-        var input = new Scanner(in);
-        out.println("Please enter the number of the task you would like to remove:");
+        var input = new Scanner(System.in);
+        System.out.println("Please enter the number of the task you would like to remove:");
         var taskNumber = input.nextLine();
         int index;
         if (NumberUtils.isParsable(taskNumber)) {
             index = Integer.parseInt(taskNumber);
         } else {
-            out.println("Invalid task number format");
+            System.out.println("Invalid task number format");
             return;
         }
         // taskNumber is indexed with -1 because it's displayed from 1 to x in the method taskList
@@ -123,11 +121,11 @@ public class Main {
             if (index-1 < tasks.length) {
                 tasks = ArrayUtils.remove(tasks, index-1);
             } else {
-                out.println("Invalid task number");
+                System.out.println("Invalid task number");
             }
         } catch (ArrayIndexOutOfBoundsException e) {
-            err.println("Invalid task number");
-            e.printStackTrace(err);
+            System.err.println("Invalid task number");
+            e.printStackTrace(System.err);
         }
     }
 
@@ -141,8 +139,8 @@ public class Main {
         try {
             Files.write(file, Arrays.asList(line));
         } catch (IOException e) {
-            err.println("Error writing file.");
-            e.printStackTrace(err);
+            System.err.println("Error writing file.");
+            e.printStackTrace(System.err);
         }
     }
 }
